1a711b404f5cdab92b67356c62b7ec81c588c472
nexant <nihant.g@gmail.com>
2014-10-19 16:29:17 -0600

    Update List.java
    Added add,remove methods

--- List.java
+++ List.java
@@ -1,1 +1,45 @@
 // Create List getters and setters here
+class Link {
+  
+  // link is head here
+  
+  private Node link;
+  public Link() {
+    link = new Node(link);
+    count = 0;
+  }
+  
+  public void addLink(Object link, int index) {
+    Node temp = new Node(link);
+    Node current = link;
+    for (int i=0; i < index; i++) {
+      current = current.getNext();
+    }
+    current.setNext(temp);
+    temp.setNext(current.getNext());
+    count++;
+  }
+  
+  public boolean remove(int index) {
+    if(index < 1 || index > size()) 
+      return false;
+      
+    for (int i = 0; i < index; i++) {
+      current = current.getNext());
+      }
+      current.setNext(current.getNext().getNext()); // node1 -> node2 -> node3 = node1 -> node3 (for node2 deletion)
+      count--;
+      return true;
+  }
+  
+  public Link getNext() {
+    return next;
+  }
+  
+  public void setNext(Node myNext) {
+   next = myNext; 
+  }
+  
+  
+  
+}
